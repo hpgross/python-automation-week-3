282e8c01a8075503c9ce7cb853b2b64e1ea7c85d
hpgross <hpgross@gmail.com>
2021-05-17 07:48:49 -0400

    Changed sender and added an entry in example.py
    sender@example.com was changed to automation@example.com
    
    An entry for kiwis was added to the fruit data table

--- scripts/example.py
+++ scripts/example.py
@@ -13,9 +13,10 @@ table_data=[
   ['bananas', 5, 1.99],
   ['cherries', 23, 5.80],
   ['grapes', 13, 2.48]]
+  ['kiwi', 4, 0.49]
 reports.generate("/tmp/report.pdf", "A Complete Inventory of My Fruit", "This is all my fruit.", table_data)
 
-sender = "sender@example.com"
+sender = "automation@example.com"
 receiver = "{}@example.com".format(os.environ.get('USER'))
 subject = "List of Fruits"
 body = "Hi\n\nI'm sending an attachment with all my fruit."
